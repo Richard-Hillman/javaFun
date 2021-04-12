9643c20261c6860152a9a6f3a7882554c74935f6
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-04-12 11:24:14 -0700

    types in JAva

---
+++ javaHello/hello/src/Type.java
@@ -0,0 +1,21 @@
+public class Type {
+    
+
+	public static void main(String[] args) {
+		int myNumber = 7; 
+		short myShortNumber = 32000;
+		long myLongNumber = 9449;
+				
+		double extraPrecise = 7.49232; 
+		float floatingNumber = 32434.4f;
+		
+		char myCharacter = 'a';
+		boolean trueFalse = false;
+		
+		byte myByte = 127;
+		
+				
+		System.out.println(myNumber);
+		System.out.println(myShortNumber);
+	}
+}
