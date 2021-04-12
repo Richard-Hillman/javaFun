64c33eaffaa8dac87740ece91c9614a8d48b0331
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-04-12 13:35:08 -0700

    strings

---
+++ javaHello/hello/src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(String[] args) {
+        String stringText = "hello";
+        String stringOne = "rambler";
+
+        String stringTogether = stringText + stringOne;
+         
+        System.out.println(stringTogether);
+        System.out.println(stringText);
+    }
+}
