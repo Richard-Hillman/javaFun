c877c4cc1a695da3b21ce2bd93f341b8aac010d4
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-04-15 20:48:36 -0700

    if statement

---
+++ javaHello/hello/src/ifStatement.java
@@ -0,0 +1,14 @@
+import jdk.tools.jlink.internal.SymLinkResourcePoolEntry;
+
+public class ifStatement {
+    public static void main(String[] args) {
+
+        if( 8 <= 6 ) { 
+        System.out.println("yup");
+        }
+        else if( 91 >= 11 ) {
+        System.out.println("nope");
+        }
+
+    }
+}
